d997874d663b51e1d1968f8cb139d057e5146ae5
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-07-05 16:17:42 +0530

    lifted up the state to App component. Show status message to the user, calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react';
 import './styles.scss';
+import { useState } from 'react';
 import Borard from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
+   const [squares, setSquares] = useState(Array(9).fill(null));
+   const [isXNext, setIsXNext] = useState(false);
+
+   const winner = calculateWinner(squares);
+   const nextPlayer = isXNext ? 'X' : 'O';
+   const statusMessage = winner
+      ? `Winner is ${winner}`
+      : `Next player is ${nextPlayer}`;
+
+   const handleSquareClick = clickedPosition => {
+      // null, X, O
+      if (squares[clickedPosition] || winner) {
+         return;
+      }
+
+      setSquares(currentSquares => {
+         return currentSquares.map((squareValue, position) => {
+            if (clickedPosition === position) {
+               return isXNext ? 'X' : 'O';
+            }
+
+            return squareValue;
+         });
+      });
+
+      setIsXNext(currentIsXNext => !currentIsXNext);
+   };
+
    return (
       <div className="app">
-         <Borard />
+         <h2>{statusMessage}</h2>
+         <Borard squares={squares} handleSquareClick={handleSquareClick} />
       </div>
    );
 }
